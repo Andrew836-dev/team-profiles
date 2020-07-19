f89419ffe9db6eaf79c72f37c2158c03617e8760
Andrew Gray <andrew.aj.gray@gmail.com>
2020-07-19 09:45:01 +0930

    completed base functionality

--- app.js
+++ app.js
@@ -86,33 +86,27 @@ function pushAndAskForMore(employee) {
         type: "confirm",
         message: `${employees.length} total employees. Add another?`,
         name: "anotherEmployee"
-    }]).then(({ anotherEmployee }) => (anotherEmployee ? promptForEmployeeData() : finishedQuestions()));
+    }]).then(({ anotherEmployee }) => (anotherEmployee ? promptForEmployeeData() : createHTML()));
 }
 
-function finishedQuestions() {
-    
+function createHTML() {
     console.log("Grats!");
-    console.log(render(employees));
+    const outputData = render(employees);
+    if (!fs.existsSync(outputPath)) {
+        console.log("Creating output Path");
+        fs.mkdirSync(OUTPUT_DIR);
+    }
+    else {
+        console.log("Output Directory exists");
+    }
+    console.log("Creating HTML file");
+    fs.writeFileSync(outputPath, outputData);
 }
 
-promptForEmployeeData()
+function init(){
+    console.log("Enter the details for your employees and a summary html file will be created");
+    promptForEmployeeData();
+}
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+init();
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
